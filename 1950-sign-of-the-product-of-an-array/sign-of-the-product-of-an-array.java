79f67fbbe734f59fc3bb4edfc055754c276103c8
Ridhima <144834787+Ridhima17342@users.noreply.github.com>
2026-06-22 12:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (56.00%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int arraySign(int[] nums) {
+       int pos = 0;
+       int neg = 0;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==0){
+            return 0;
+        }
+        else if(nums[i]<0){
+            neg++;
+        }
+        else{
+            pos++;
+        }
+       }
+       if(neg % 2==0){
+        return 1;
+       }
+       return -1;
+    }
+}
